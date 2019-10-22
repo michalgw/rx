4acf6e415b506fef52884f1d5e8b5766118319d5
alexs75 <alexs75@8e941d3f-bd1b-0410-a28a-d453659cc2b4>
2019-10-22 12:23:12 +0000

    RxFPC:demo for RxDBGrid filter values
    git-svn-id: https://svn.code.sf.net/p/lazarus-ccr/svn/components/rx/trunk@7166 8e941d3f-bd1b-0410-a28a-d453659cc2b4

--- demos/rxdbgrid/RxDBGrid_Filter/unit1.pas
+++ demos/rxdbgrid/RxDBGrid_Filter/unit1.pas
@@ -6,20 +6,31 @@ interface
 
 uses
   Classes, SysUtils, FileUtil, rxdbgrid, rxmemds, Forms, Controls, Graphics,
-  Dialogs, StdCtrls, db;
+  Dialogs, StdCtrls, ExtCtrls, db;
 
 type
 
   { TForm1 }
 
   TForm1 = class(TForm)
+    Button1: TButton;
+    Button2: TButton;
+    Button3: TButton;
     dsData: TDataSource;
+    Label1: TLabel;
+    Label2: TLabel;
+    Label3: TLabel;
+    Panel1: TPanel;
     rxDataID_R: TLongintField;
     RxDBGrid1: TRxDBGrid;
     rxData: TRxMemoryData;
     rxDataCODE: TLongintField;
     rxDataID: TLongintField;
     rxDataNAME: TStringField;
+    Splitter1: TSplitter;
+    procedure Button1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
+    procedure Button3Click(Sender: TObject);
     procedure FormCreate(Sender: TObject);
     procedure rxDataAfterInsert(DataSet: TDataSet);
   private
@@ -49,6 +60,25 @@ begin
       rxData.AppendRecord([null, null, 'Пустая строка']);
   end;
   rxData.First;
+
+  Label1.Caption:='';
+  Label2.Caption:='';
+  Label3.Caption:='';
+end;
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  Label1.Caption:=RxDBGrid1.ColumnByFieldName('CODE').Filter.CurrentValues.Text;
+end;
+
+procedure TForm1.Button2Click(Sender: TObject);
+begin
+  Label2.Caption:=RxDBGrid1.ColumnByFieldName('NAME').Filter.CurrentValues.Text;
+end;
+
+procedure TForm1.Button3Click(Sender: TObject);
+begin
+  Label3.Caption:=RxDBGrid1.ColumnByFieldName('ID_R').Filter.CurrentValues.Text;
 end;
 
 procedure TForm1.rxDataAfterInsert(DataSet: TDataSet);
